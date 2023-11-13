28e4da295aa576d5f721bd0cba76242fc96cb2cd
Alex North <445306+anorth@users.noreply.github.com>
2023-11-14 09:20:50 +1300

    Granite clean-ups, fix receiving own CONVERGE.

--- granite/granite.go
+++ granite/granite.go
@@ -129,9 +129,21 @@ type instance struct {
 }
 
 func newInstance(ntwk net.NetworkSink, participantID string, instanceID int, delta float64, input net.ECChain) *instance {
-	return &instance{ntwk: ntwk, participantID: participantID, instanceID: instanceID, delta: delta, input: input,
-		converged: map[int][]GMessage{}, quality: []GMessage{}, prepared: map[int]map[string]net.ECChain{},
-		committed: map[int]map[string]net.ECChain{}}
+	return &instance{
+		ntwk:          ntwk,
+		participantID: participantID,
+		instanceID:    instanceID,
+		delta:         delta,
+		input:         input,
+		round:         0,
+		phase:         "",
+		proposal:      input,
+		value:         net.ECChain{},
+		quality:       []GMessage{},
+		converged:     map[int][]GMessage{},
+		prepared:      map[int]map[string]net.ECChain{},
+		committed:     map[int]map[string]net.ECChain{},
+	}
 }
 
 func (i *instance) start() {
@@ -139,29 +151,28 @@ func (i *instance) start() {
 }
 
 func (i *instance) receive(sender string, msg GMessage) {
-	valueValid := func(msg *GMessage) bool {
-		return msg.Bottom || msg.Value.Base.Eq(&i.input.Base)
-	}
 	round := msg.Round
-	if msg.Step == QUALITY && valueValid(&msg) {
+	if msg.Step == QUALITY && msg.Value.Base.Eq(&i.input.Base) {
 		// Just collect all the messages until the alarm triggers the end of QUALITY phase.
 		// Note the message will be collected but ignored if QUALITY timeout has already passed.
 		i.quality = append(i.quality, msg)
-	} else if msg.Step == CONVERGE && valueValid(&msg) {
+	} else if msg.Step == CONVERGE && msg.Value.Base.Eq(&i.input.Base) {
 		// Collect messages until the alarm triggers the end of CONVERGE phase.
 		i.converged[round] = append(i.converged[round], msg)
-	} else if msg.Step == PREPARE && valueValid(&msg) {
+	} else if msg.Step == PREPARE && msg.Value.Base.Eq(&i.input.Base) {
 		if _, exists := i.prepared[round]; !exists {
 			i.prepared[round] = make(map[string]net.ECChain)
 		}
 		i.prepared[round][sender] = msg.Value
 		i.tryPrepare()
-	} else if msg.Step == COMMIT && valueValid(&msg) {
+	} else if msg.Step == COMMIT && (msg.Bottom || msg.Value.Base.Eq(&i.input.Base)) {
 		if _, exists := i.committed[round]; !exists {
 			i.committed[round] = make(map[string]net.ECChain)
 		}
 		i.committed[round][sender] = msg.Value
 		i.tryCommit()
+	} else {
+		panic(fmt.Sprintf("unexpected message %v", msg))
 	}
 }
 
@@ -176,97 +187,34 @@ func (i *instance) receiveAlarm() {
 func (i *instance) beginQuality() {
 	// Broadcast proposal value and wait 2Δ to receive from others.
 	i.phase = QUALITY
-	i.broadcast(QUALITY, i.input)
+	msg := i.broadcast(QUALITY, i.proposal)
+	i.receive(i.participantID, msg)
 	i.alarmAfter(2 * i.delta)
 }
 
 func (i *instance) endQuality() {
-	// QUALITY phase ends.
-	// Calculate the set of allowed proposals, and then find the best one.
-	type candidate struct {
-		chain net.ECChain
-		power uint
-	}
-	base := i.input.Base
-	// Candidate chains indexed by final tipset CID.
-	candidates := map[net.CID]candidate{}
-	// Add non-empty prefixes of own input chain as a candidates.
-	for j := range i.input.Suffix {
-		prefix := i.input.Prefix(j + 1)
-		candidates[prefix.Head().CID] = candidate{*prefix, base.PowerTable.Entries[i.participantID]}
-	}
-	// Add power to candidates from messages received.
-	for _, msg := range i.quality {
-		for j := range msg.Value.Suffix {
-			prefix := msg.Value.Prefix(j + 1)
-			if found, ok := candidates[prefix.Head().CID]; ok {
-				candidates[prefix.Head().CID] = candidate{
-					chain: found.chain,
-					power: found.power + base.PowerTable.Entries[msg.Sender],
-				}
-			} else {
-				// XXX: If the tipset isn't in our input chain, we can't verify its weight or power table.
-				// This boils down to just trusting the other nodes to have computed it correctly.
-				candidates[prefix.Head().CID] = candidate{*prefix, base.PowerTable.Entries[msg.Sender]}
-			}
-		}
-	}
-
-	// Filter received tipsets to those with more than half of power in favour.
-	threshold := base.PowerTable.Total / 2
-	allowed := []candidate{}
-	for _, c := range candidates {
-		if c.power > threshold {
-			allowed = append(allowed, c)
-		}
-	}
-	// Sort allowed candidates by tipset weight, descending.
-	sort.Slice(allowed, func(i, j int) bool {
-		hi := allowed[i].chain.Head()
-		hj := allowed[j].chain.Head()
-		return hi.Compare(hj) > 0
-	})
-
-	if len(allowed) > 0 {
-		// XXX: This can cause a participant to vote for a chain that is not its heaviest,
-		// or that it can't even validate, which is irrational.
-		// TODO: detect and log this.
-		i.proposal = allowed[0].chain
-	} else {
-		i.proposal = *i.input.BaseChain()
-	}
+	i.proposal = findBestQualityProposal(i.quality, i.input.Base)
 	i.value = i.proposal
 	i.beginPrepare()
 }
 
 func (i *instance) beginConverge() {
 	i.phase = CONVERGE
-	i.broadcast(CONVERGE, i.proposal)
+	msg := i.broadcast(CONVERGE, i.proposal)
+	i.receive(i.participantID, msg)
 	i.alarmAfter(2 * i.delta)
 }
 
 func (i *instance) endConverge() {
-	var minTicket []byte
-	var minValue net.ECChain
-	// Emulate a ticket draw by hashing the sender and round number.
-	for _, v := range i.converged[i.round] {
-		input := fmt.Sprintf("%s%d", v.Sender, i.round)
-		digest := sha256.Sum224([]byte(input))
-		if minTicket == nil || bytes.Compare(digest[:], minTicket[:]) < 0 {
-			minTicket = digest[:]
-			minValue = v.Value
-		}
-	}
-	i.value = minValue
+	i.value = findMinTicketProposal(i.converged[i.round], i.round)
 	i.beginPrepare()
 }
 
 func (i *instance) beginPrepare() {
 	// Broadcast preparation of value and wait for everyone to respond.
 	i.phase = PREPARE
-	i.broadcast(PREPARE, i.value)
-	// Check whether we've already received enough PREPARE messages to proceed.
-	i.tryPrepare()
+	msg := i.broadcast(PREPARE, i.value)
+	i.receive(i.participantID, msg)
 }
 
 func (i *instance) tryPrepare() {
@@ -284,9 +232,8 @@ func (i *instance) tryPrepare() {
 
 func (i *instance) beginCommit() {
 	i.phase = COMMIT
-	i.broadcast(COMMIT, i.value)
-	// Check whether we've already received enough COMMIT messages to decide.
-	i.tryCommit()
+	msg := i.broadcast(COMMIT, i.value)
+	i.receive(i.participantID, msg)
 }
 
 func (i *instance) tryCommit() {
@@ -329,9 +276,11 @@ func (p *Participant) decided() bool {
 	return p.granite != nil && p.granite.phase == DECIDE
 }
 
-func (i *instance) broadcast(step string, msg net.ECChain) {
+func (i *instance) broadcast(step string, msg net.ECChain) GMessage {
 	bottom := msg.Eq(&net.ECChain{})
-	i.ntwk.Broadcast(i.participantID, GMessage{i.instanceID, i.round, i.participantID, step, msg, bottom})
+	gmsg := GMessage{i.instanceID, i.round, i.participantID, step, msg, bottom}
+	i.ntwk.Broadcast(i.participantID, gmsg)
+	return gmsg
 }
 
 func (i *instance) alarmAfter(delay float64) {
@@ -343,28 +292,93 @@ func (i *instance) log(format string, args ...interface{}) {
 	i.ntwk.Log("%s/%d: %v", i.participantID, i.instanceID, msg)
 }
 
+///// Helpers
+
+// Finds the highest weight chain prefix with at least half of power in favour.
+func findBestQualityProposal(proposals []GMessage, base net.TipSet) net.ECChain {
+	// Calculate the set of prefixes with sufficient weight, and then find the best one.
+	type candidate struct {
+		chain net.ECChain
+		power uint
+	}
+	// Candidate chains indexed by final tipset CID.
+	candidates := map[net.CID]candidate{}
+	// Add power to candidates from messages received.
+	for _, msg := range proposals {
+		for j := range msg.Value.Suffix {
+			prefix := msg.Value.Prefix(j + 1)
+			if found, ok := candidates[prefix.Head().CID]; ok {
+				candidates[prefix.Head().CID] = candidate{
+					chain: found.chain,
+					power: found.power + base.PowerTable.Entries[msg.Sender],
+				}
+			} else {
+				candidates[prefix.Head().CID] = candidate{*prefix, base.PowerTable.Entries[msg.Sender]}
+			}
+		}
+	}
+
+	// Filter received tipsets to those with strictly more than half of power in favour.
+	threshold := base.PowerTable.Total / 2
+	allowed := []candidate{}
+	for _, c := range candidates {
+		if c.power > threshold {
+			allowed = append(allowed, c)
+		}
+	}
+	// Sort allowed candidates by tipset weight, descending.
+	sort.Slice(allowed, func(i, j int) bool {
+		hi := allowed[i].chain.Head()
+		hj := allowed[j].chain.Head()
+		return hi.Compare(hj) > 0
+	})
+
+	var proposal net.ECChain
+	if len(allowed) > 0 {
+		// XXX: If a tipset isn't in our input chain, we can't verify its weight or power table.
+		// This boils down to just trusting the other nodes to have computed it correctly.
+		// This can cause a participant to vote for a chain that is not its heaviest,
+		// or that it can't even validate, which is irrational.
+		// TODO: detect and log this.
+		proposal = allowed[0].chain
+	} else {
+		proposal = *net.NewChain(base)
+	}
+	return proposal
+}
+
+// Finds the lowest ticket value from a set of proposals.
+func findMinTicketProposal(proposals []GMessage, round int) net.ECChain {
+	var minTicket []byte
+	var minValue net.ECChain
+	// Emulate a ticket draw by hashing the sender and round number.
+	for _, v := range proposals {
+		input := fmt.Sprintf("%s%d", v.Sender, round)
+		digest := sha256.Sum224([]byte(input))
+		if minTicket == nil || bytes.Compare(digest[:], minTicket[:]) < 0 {
+			minTicket = digest[:]
+			minValue = v.Value
+		}
+	}
+	return minValue
+}
+
 // Returns whether the proposals constitute a quorum of voting power, and if so, either the quorum chain or empty (bottom).
 func findQuorum(me string, mine net.ECChain, proposals map[string]net.ECChain, power net.PowerTable) (bool, net.ECChain) {
 	threshold := power.Total * 2 / 3
-	// Initialise mapping of tipset->power with preferred proposal.
-	votingPower := power.Entries[me]
-	powers := map[net.CID]uint{
-		mine.Head().CID: power.Entries[me],
-	}
-	// Mapping of chain tips to chains.
-	chains := map[net.CID]net.ECChain{
-		mine.Head().CID: mine,
-	}
+	votingPower := uint(0)
+	tipsetPower := map[net.CID]uint{}
+	tipsetChain := map[net.CID]net.ECChain{}
 
 	for sender, proposal := range proposals {
-		powers[proposal.Head().CID] += power.Entries[sender]
-		chains[proposal.Head().CID] = proposal
+		tipsetPower[proposal.Head().CID] += power.Entries[sender]
+		tipsetChain[proposal.Head().CID] = proposal
 		votingPower += power.Entries[sender]
 	}
 	// If any proposal has more than 2/3 of power, return it.
-	for cid, power := range powers {
+	for cid, power := range tipsetPower {
 		if power > threshold {
-			chain := chains[cid]
+			chain := tipsetChain[cid]
 			return true, chain
 		}
 	}
