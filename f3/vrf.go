a1f8da18926dc037568ea378cef1d405b9c8506f
Alejandro Ranchal-Pedrosa <a.ranchalpedrosa@gmail.com>
2023-12-20 16:34:48 +0100

    Implement VRF ticket creation/verification

--- f3/vrf.go
+++ f3/vrf.go
@@ -2,6 +2,7 @@ package f3
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 )
 
@@ -21,6 +22,40 @@ type VRFTicketSource interface {
 
 type VRFTicketVerifier interface {
 	VerifyTicket(beacon []byte, instance uint32, round uint32, signer ActorID, ticket Ticket) bool
+}
+
+// VRF used for the CONVERGE step of GossiPBFT.
+type VRF struct {
+	signer Signer
+}
+
+func NewVRF(signer Signer) *VRF {
+	return &VRF{
+		signer: signer,
+	}
+}
+
+func (f *VRF) MakeTicket(beacon []byte, instance uint32, round uint32, signer ActorID) Ticket {
+	return f.signer.Sign(signer, f.serializeSigInput(beacon, instance, round))
+}
+
+func (f *VRF) VerifyTicket(beacon []byte, instance uint32, round uint32, signer ActorID, ticket Ticket) bool {
+	return f.signer.Verify(signer, f.serializeSigInput(beacon, instance, round), ticket)
+}
+
+// Serializes the input to the VRF signature for the CONVERGE step of GossiPBFT.
+// Only used for VRF ticket creation and/or verification.
+func (f *VRF) serializeSigInput(beacon []byte, instance uint32, round uint32) []byte {
+	instanceBytes := make([]byte, 4)
+	roundBytes := make([]byte, 4)
+
+	binary.BigEndian.PutUint32(instanceBytes, instance)
+	binary.BigEndian.PutUint32(roundBytes, round)
+
+	sigInput := append(beacon, instanceBytes...)
+	sigInput = append(sigInput, roundBytes...)
+
+	return sigInput
 }
 
 type FakeVRF struct {
