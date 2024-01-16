dbf1545f1ea1d787bbf472c7d218f8cf7b5cd236
Alejandro Ranchal-Pedrosa <a.ranchalpedrosa@gmail.com>
2024-01-16 11:16:58 +0100

    Merge and address comments

--- sim/network.go
+++ sim/network.go
@@ -176,7 +176,7 @@ func (n *Network) Aggregate(sig []byte, aggSignature []byte) []byte {
 	return updatedAggSignature
 }
 
-func (n *Network) VerifyAggregate(payload, aggSig []byte, signers map[f3.ActorID]struct{}) bool {
+func (n *Network) VerifyAggregate(payload, aggSig []byte, signers [][]byte) bool {
 	aggBuf := bytes.NewReader(aggSig)
 
 	verifiedSigners := make(map[f3.ActorID]struct{})
@@ -220,7 +220,7 @@ func (n *Network) VerifyAggregate(payload, aggSig []byte, signers map[f3.ActorID
 		verifiedSigners[actorID] = struct{}{}
 	}
 
-	// Ensure all signers in the bitset are accounted for.
+	// Ensure all signers are accounted for.
 	return len(verifiedSigners) == len(signers)
 }
 
