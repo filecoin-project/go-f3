fccda13ce08a66680235b28ecf4e19ea0504c5a5
Steven Allen <steven@stebalien.com>
2024-05-17 12:50:13 -0700

    Update mockery and fix file name (#250)
    Previously, `make generate` would create a file named
    `mock_Host_test.go`. Now it creates `mock_host_test.go`.

--- gpbft/mock_gen.go
+++ gpbft/mock_gen.go
@@ -1,3 +1,3 @@
 package gpbft
 
-//go:generate go run github.com/vektra/mockery/v2@v2.42.1 --name Host --testonly --inpackage --with-expecter
+//go:generate go run github.com/vektra/mockery/v2@v2.43.1 --filename mock_host_test.go --name Host --testonly --inpackage --with-expecter
