a5b82d3f8a1a405c9efe026514f3f84f96278f42
Masih H. Derkani <m@derkani.org>
2024-05-21 12:38:04 +0100

    Drop spammable messages beyond max allowed lookahead rounds
    The gPBFT rebroadcast design revision introduced an improvement whereby
    "spammable" messages, i.e. `COMMIT` for bottom, for future rounds should
    be dropped by the participants. The specification leaves room for
    implementers to optionally retain some number of such messages to
    reduce reliance on the need for rebroadcast and ultimately help reach
    consensus in fewer rounds.
    
    The work here:
    1. introduces `WithMaxLookaheadRounds` configuration option to `gpbft`
       package, defaulting to zero if unset.
    2. drops all spammable messages belonging to rounds that are beyond the
       configured max lookahead rounds.
    
    A `Spam` adversary is introduced to replicate the conditions at which
    max lookahead rounds would start dropping messages. A set of tests then
    use the `Spam` adversary to assert that honest participants reach
    expected consensus despite the presence of spam messages.
    
    Note that there are no APIs to assert that the upper lookahead bound is
    respected by the implementation. A TODO is left to revisit this once
    telemetry is introduced whereby tests can observe future message queue
    size metrics to assert a configured lookahead is indeed respected.
    
    Resolves #240

---
+++ test/spam_test.go
@@ -0,0 +1,81 @@
+package test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+	"time"
+
+	"github.com/filecoin-project/go-f3/gpbft"
+	"github.com/filecoin-project/go-f3/sim"
+	"github.com/filecoin-project/go-f3/sim/adversary"
+	"github.com/filecoin-project/go-f3/sim/latency"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSpamAdversary(t *testing.T) {
+	t.Parallel()
+	const (
+		instanceCount = 2000
+		maxRounds     = 30
+	)
+	honestCounts := []int{3, 4, 5, 6, 7, 8, 9}
+	tests := []struct {
+		name               string
+		maxLookaheadRounds uint64
+		spamAheadRounds    uint64
+	}{
+		{
+			name:            "zero lookahead 10x spam",
+			spamAheadRounds: 10,
+		},
+		{
+			name:               "10 lookahead 3x spam",
+			maxLookaheadRounds: 10,
+			spamAheadRounds:    30,
+		},
+	}
+	for _, test := range tests {
+		for _, hc := range honestCounts {
+			test := test
+			hc := hc
+			lessThanOneThirdAdversaryStoragePower := gpbft.NewStoragePower(int64(max(hc/3-1, 1)))
+			name := fmt.Sprintf("%s honest count %d", test.name, hc)
+			t.Run(name, func(t *testing.T) {
+				ecChainGenerator := sim.NewUniformECChainGenerator(651651, 1, 10)
+				sm, err := sim.NewSimulation(
+					sim.WithLatencyModel(latency.NewLogNormal(455454, time.Second)),
+					sim.WithECEpochDuration(EcEpochDuration),
+					sim.WitECStabilisationDelay(EcStabilisationDelay),
+					sim.WithGpbftOptions(testGpbftOptions...),
+					sim.AddHonestParticipants(hc, ecChainGenerator, uniformOneStoragePower),
+					sim.WithGpbftOptions(gpbft.WithMaxLookaheadRounds(test.maxLookaheadRounds)),
+					sim.WithAdversary(adversary.NewSpamGenerator(lessThanOneThirdAdversaryStoragePower, test.spamAheadRounds)),
+				)
+				require.NoError(t, err)
+				require.NoErrorf(t, sm.Run(instanceCount, maxRounds), "%s", sm.Describe())
+
+				instance := sm.GetInstance(0)
+				require.NotNil(t, instance, "instance 0")
+				latestBaseECChain := instance.BaseChain
+				for i := uint64(0); i < instanceCount; i++ {
+					instance = sm.GetInstance(i + 1)
+					require.NotNil(t, instance, "instance %d", i)
+					wantDecision := ecChainGenerator.GenerateECChain(i, *latestBaseECChain.Head(), math.MaxUint64)
+
+					// Sanity check that the expected decision is progressed from the base chain
+					require.Equal(t, wantDecision.Base(), latestBaseECChain.Head())
+					require.NotEqual(t, wantDecision.Suffix(), latestBaseECChain.Suffix())
+
+					// Assert the consensus is reached at the head of expected chain despite the spam.
+					requireConsensusAtInstance(t, sm, i, wantDecision...)
+					latestBaseECChain = instance.BaseChain
+				}
+
+				// TODO: We have no good way of asserting that the spam is mitigated as DoS
+				//       vector via maxLookaheadRounds. Expand assertions of this test via
+				//       metrics once observability/telemetry is implemented.
+			})
+		}
+	}
+}
