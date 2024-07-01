81427cfcb229586c3231fbd304b74bb4cf2d54ab
Steven Allen <steven@stebalien.com>
2024-06-30 18:20:43 -0700

    basic client/server test

---
+++ certexchange/protocol_test.go
@@ -0,0 +1,153 @@
+package certexchange_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/filecoin-project/go-f3/certexchange"
+	"github.com/filecoin-project/go-f3/certs"
+	"github.com/filecoin-project/go-f3/certstore"
+	"github.com/filecoin-project/go-f3/gpbft"
+
+	"github.com/ipfs/go-datastore"
+	ds_sync "github.com/ipfs/go-datastore/sync"
+	logging "github.com/ipfs/go-log/v2"
+	mocknetwork "github.com/libp2p/go-libp2p/p2p/net/mock"
+	"github.com/stretchr/testify/require"
+)
+
+const testNetworkName gpbft.NetworkName = "testnet"
+
+var log = logging.Logger("certexchange-test")
+
+func testPowerTable(entries int64) (gpbft.PowerEntries, gpbft.CID) {
+	powerTable := make(gpbft.PowerEntries, entries)
+
+	for i := range powerTable {
+		powerTable[i] = gpbft.PowerEntry{
+			ID:     gpbft.ActorID(i + 1),
+			Power:  gpbft.NewStoragePower(int64(len(powerTable)*2 - i/2)),
+			PubKey: []byte("fake key"),
+		}
+	}
+	k, err := certs.MakePowerTableCID(powerTable)
+	if err != nil {
+		panic(err)
+	}
+	return powerTable, k
+}
+
+func TestClientServer(t *testing.T) {
+	mocknet := mocknetwork.New()
+	h1, err := mocknet.GenPeer()
+	require.NoError(t, err)
+	h2, err := mocknet.GenPeer()
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	require.NoError(t, mocknet.LinkAll())
+
+	ds := ds_sync.MutexWrap(datastore.NewMapDatastore())
+	pt, pcid := testPowerTable(10)
+	supp := gpbft.SupplementalData{PowerTable: pcid}
+
+	cs, err := certstore.CreateStore(ctx, ds, 0, pt)
+	require.NoError(t, err)
+
+	cert := &certs.FinalityCertificate{GPBFTInstance: 0, SupplementalData: supp}
+	err = cs.Put(ctx, cert)
+	require.NoError(t, err)
+
+	cert = &certs.FinalityCertificate{GPBFTInstance: 1, SupplementalData: supp}
+	err = cs.Put(ctx, cert)
+	require.NoError(t, err)
+
+	server := certexchange.Server{
+		NetworkName: testNetworkName,
+		Host:        h1,
+		Store:       cs,
+		Log:         log,
+	}
+
+	client := certexchange.Client{
+		Host:        h2,
+		NetworkName: testNetworkName,
+		Log:         log,
+	}
+
+	require.NoError(t, server.Start())
+	defer server.Stop()
+
+	require.NoError(t, mocknet.ConnectAllButSelf())
+
+	// No certificates, but get the power table.
+	{
+		head, certs, err := client.Request(ctx, h1.ID(), &certexchange.Request{
+			FirstInstance:     0,
+			Limit:             0,
+			IncludePowerTable: true,
+		})
+		require.NoError(t, err)
+		require.EqualValues(t, 2, head.PendingInstance)
+		require.EqualValues(t, pt, head.PowerTable)
+		require.Empty(t, certs) // limit == 0
+	}
+
+	// All certificates and no power table.
+	{
+		head, certs, err := client.Request(ctx, h1.ID(), &certexchange.Request{
+			FirstInstance:     0,
+			Limit:             certexchange.NoLimit,
+			IncludePowerTable: false,
+		})
+
+		require.NoError(t, err)
+		require.EqualValues(t, 2, head.PendingInstance)
+		require.Nil(t, head.PowerTable)
+
+		expectInstance := uint64(0)
+		for c := range certs {
+			require.Equal(t, expectInstance, c.GPBFTInstance)
+			expectInstance++
+		}
+		require.Equal(t, expectInstance, head.PendingInstance)
+	}
+
+	// Just one certificate
+	{
+		_, certs, err := client.Request(ctx, h1.ID(), &certexchange.Request{
+			FirstInstance: 0,
+			Limit:         1,
+		})
+		require.NoError(t, err)
+
+		require.Equal(t, uint64(0), (<-certs).GPBFTInstance)
+		require.Empty(t, certs)
+	}
+
+	// Should be able to get the power table beyond the last instance.
+	{
+		head, certs, err := client.Request(ctx, h1.ID(), &certexchange.Request{
+			FirstInstance:     2,
+			Limit:             certexchange.NoLimit,
+			IncludePowerTable: true,
+		})
+		require.NoError(t, err)
+		require.EqualValues(t, pt, head.PowerTable)
+		require.Empty(t, certs)
+	}
+
+	// Should get nothing beyond that
+	{
+		head, certs, err := client.Request(ctx, h1.ID(), &certexchange.Request{
+			FirstInstance:     3,
+			Limit:             certexchange.NoLimit,
+			IncludePowerTable: true,
+		})
+		require.NoError(t, err)
+		require.Nil(t, head.PowerTable)
+		require.Empty(t, certs)
+	}
+}
