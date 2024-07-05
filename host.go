19541925b2a14fdc688b9a0bedfa9c7b65053c39
Steven Allen <steven@stebalien.com>
2024-07-05 14:34:02 +0200

    fixup merge issue

--- host.go
+++ host.go
@@ -155,7 +155,7 @@ func (h *gpbftRunner) computeNextInstanceStart(cert *certs.FinalityCertificate) 
 		// we decided on something new, the tipset that got finalized can at minimum be 30-60s old.
 		return ts.Timestamp().Add(ecDelay)
 	}
-	if decision.Vote.Instance == manifest.InitialInstance {
+	if cert.GPBFTInstance == manifest.InitialInstance {
 		// if we are at initial instance, there is no history to look at
 		return ts.Timestamp().Add(ecDelay)
 	}
