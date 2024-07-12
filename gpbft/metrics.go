2782a3afabc28240dcf8c8ddb46f0b2416556bd6
Masih H. Derkani <m@derkani.org>
2024-07-12 16:47:54 +0200

    Add metrics for error counts tagged by type (#456)
    Add metrics that count the number of errors returned by the exported
    API.

--- gpbft/metrics.go
+++ gpbft/metrics.go
@@ -1,12 +1,17 @@
 package gpbft
 
 import (
+	"errors"
+
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 )
 
-const attrKeyPhase = "phase"
+const (
+	attrKeyPhase = "phase"
+	attrKeyErr   = "err"
+)
 
 var (
 	meter = otel.Meter("f3/gpbft")
@@ -25,6 +30,7 @@ var (
 		broadcastCounter          metric.Int64Counter
 		reBroadcastCounter        metric.Int64Counter
 		reBroadcastAttemptCounter metric.Int64Counter
+		errorCounter              metric.Int64Counter
 	}
 )
 
@@ -36,9 +42,38 @@ func init() {
 	))
 	metrics.broadcastCounter = must(meter.Int64Counter("broadcast_counter", metric.WithDescription("Number of broadcasted messages")))
 	metrics.reBroadcastCounter = must(meter.Int64Counter("rebroadcast_counter", metric.WithDescription("Number of rebroadcasted messages")))
-	metrics.reBroadcastAttemptCounter = must(
-		meter.Int64Counter(
-			"rebroadcast_attempt_counter", metric.WithDescription("Number of rebroadcast attempts")))
+	metrics.reBroadcastAttemptCounter = must(meter.Int64Counter("rebroadcast_attempt_counter", metric.WithDescription("Number of rebroadcast attempts")))
+	metrics.errorCounter = must(meter.Int64Counter("error_counter", metric.WithDescription("Number of errors")))
+}
+
+func metricAttributeFromError(err error) attribute.KeyValue {
+	var v string
+	switch {
+	case errors.Is(err, ErrValidationTooOld):
+		v = "invalid_too_old"
+	case errors.Is(err, ErrValidationNoCommittee):
+		v = "invalid_no_committee"
+	case errors.Is(err, ErrValidationInvalid):
+		v = "invalid_msg"
+	case errors.Is(err, ErrValidationWrongBase):
+		v = "invalid_wrong_base"
+	case errors.Is(err, ErrValidationWrongSupplement):
+		v = "invalid_wrong_supp"
+	case errors.As(err, &ValidationError{}):
+		v = "type_invalid"
+	case errors.Is(err, ErrReceivedWrongInstance):
+		v = "wrong_instance"
+	case errors.Is(err, ErrReceivedAfterTermination):
+		v = "after_termination"
+	case errors.Is(err, ErrReceivedInternalError):
+		v = "internal"
+	case errors.Is(err, &PanicError{}):
+		// Any unknown error that ended up getting wrapped with PanicError.
+		v = "recovered_panic"
+	default:
+		v = "unknown"
+	}
+	return attribute.KeyValue{Key: attrKeyErr, Value: attribute.StringValue(v)}
 }
 
 func must[V any](v V, err error) V {
