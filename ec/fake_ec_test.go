738e8510a01943374d983b5549ba978a99df9808
Jakub Sztandera <oss@kubuxu.com>
2024-07-13 14:10:33 +0200

    Gracefully handle EC diverging from F3 (#462)
    * Gracefully handle EC divering from F3
    
    We will end up proposing base which will trigger backoff, giving us
    chance to re-boostrap,
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * Add test for tipset.String()
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * Add metric for the head diverged
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * Fix missed rename
    
    That is what I get for using :%s/ instead of :GoRename
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * fix typo
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * Address review
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    ---------
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

---
+++ ec/fake_ec_test.go
@@ -0,0 +1,14 @@
+package ec
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTipsetString(t *testing.T) {
+	ec := &FakeEC{}
+	ts := ec.genTipset(1)
+	require.True(t, len(ts.String()) != 0)
+	t.Log(ts.String())
+}
