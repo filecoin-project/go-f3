b2c37f59ae41edbab482a8bd645b2f5a0d08d076
Steven Allen <steven@stebalien.com>
2024-07-17 16:17:58 +0000

    Improve power-table loading logic (#481)
    * Load the power table from the certstore whenever possible
    
    * Don't load the powertable when opening the certstore after bootstrap
    
    We may not have state that far back.

--- store.go
+++ store.go
@@ -2,6 +2,7 @@ package f3
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/filecoin-project/go-f3/certstore"
@@ -15,8 +16,13 @@ import (
 // openCertstore opens the certificate store for the specific manifest (namespaced by the network
 // name).
 func openCertstore(ctx context.Context, ec ec.Backend, ds datastore.Datastore, m *manifest.Manifest) (*certstore.Store, error) {
-
 	ds = namespace.Wrap(ds, m.DatastorePrefix())
+
+	if cs, err := certstore.OpenStore(ctx, ds); err == nil {
+		return cs, nil
+	} else if !errors.Is(err, certstore.ErrNotInitialized) {
+		return nil, err
+	}
 
 	ts, err := ec.GetTipsetByEpoch(ctx, m.BootstrapEpoch-m.ECFinality)
 	if err != nil {
@@ -28,5 +34,5 @@ func openCertstore(ctx context.Context, ec ec.Backend, ds datastore.Datastore, m
 		return nil, fmt.Errorf("getting initial power table: %w", err)
 	}
 
-	return certstore.OpenOrCreateStore(ctx, ds, m.InitialInstance, initialPowerTable)
+	return certstore.CreateStore(ctx, ds, m.InitialInstance, initialPowerTable)
 }
