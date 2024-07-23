b5684981656fc03f20adc3569beb343f4127142d
Jakub Sztandera <oss@kubuxu.com>
2024-07-23 17:00:54 +0200

    Don't error out if initial manifest resume fails
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- f3.go
+++ f3.go
@@ -177,10 +177,8 @@ func (m *F3) Start(startCtx context.Context) (_err error) {
 	// Try to start immediately if we have a manifest available and don't have to wait to
 	// bootstrap. That way, we'll be fully started when we return from Start.
 	if initialDelay == 0 {
-		// FIXME: this failing will kill whole F3 and prevent it from starting
-		// dont do that
 		if err := m.reconfigure(startCtx, pendingManifest); err != nil {
-			return fmt.Errorf("failed to start GPBFT: %w", err)
+			log.Warnw("failed to reconfigure GPBFT", "error", err)
 		}
 		pendingManifest = nil
 	}
@@ -246,6 +244,7 @@ func (m *F3) Stop(stopCtx context.Context) (_err error) {
 }
 
 func (m *F3) reconfigure(ctx context.Context, manif *manifest.Manifest) (_err error) {
+	log.Infow("starting f3 reconfiguration")
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
@@ -288,6 +287,7 @@ func (m *F3) Resume() error {
 }
 
 func (m *F3) stopInternal(ctx context.Context) error {
+	log.Infow("stopping F3 internals")
 	var err error
 	if m.ps != nil {
 		if serr := m.ps.Stop(ctx); serr != nil {
@@ -319,6 +319,7 @@ func (m *F3) stopInternal(ctx context.Context) error {
 }
 
 func (m *F3) resumeInternal(ctx context.Context) error {
+	log.Infow("resuming F3 internals")
 	mPowerEc := ec.WithModifiedPower(m.ec, m.manifest.ExplicitPower, m.manifest.IgnoreECPower)
 
 	// We don't reset these fields if we only pause/resume.
