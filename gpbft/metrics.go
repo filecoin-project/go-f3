413546b41321902e942fd9c936480e9d0b2dead8
Masih H. Derkani <m@derkani.org>
2024-07-31 12:25:41 +0100

    Measure time spent on validation as seconds in float64 (#531)
    * measure time spent on validation as seconds in float64
    
    For better accuracy and compliance with otel best practices measure the
    time spent on validating messages as float64 seconds.
    
    While at it, use inline var struct instantiation instead of init to
    follow a consistent convention across the repo.
    
    * Update metrics.go
    
    ---------
    
    Co-authored-by: Jakub Sztandera <oss@kubuxu.com>

--- gpbft/metrics.go
+++ gpbft/metrics.go
@@ -33,27 +33,25 @@ var (
 		TERMINATED_PHASE: attrTerminatedPhase,
 	}
 
-	metrics struct {
+	metrics = struct {
 		phaseCounter              metric.Int64Counter
 		roundHistogram            metric.Int64Histogram
 		broadcastCounter          metric.Int64Counter
 		reBroadcastCounter        metric.Int64Counter
 		reBroadcastAttemptCounter metric.Int64Counter
 		errorCounter              metric.Int64Counter
+	}{
+		phaseCounter: must(meter.Int64Counter("f3_gpbft_phase_counter", metric.WithDescription("Number of times phases change"))),
+		roundHistogram: must(meter.Int64Histogram("f3_gpbft_round_histogram",
+			metric.WithDescription("Histogram of rounds per instance"),
+			metric.WithExplicitBucketBoundaries(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 50.0, 100.0, 1000.0),
+		)),
+		broadcastCounter:          must(meter.Int64Counter("f3_gpbft_broadcast_counter", metric.WithDescription("Number of broadcasted messages"))),
+		reBroadcastCounter:        must(meter.Int64Counter("f3_gpbft_rebroadcast_counter", metric.WithDescription("Number of rebroadcasted messages"))),
+		reBroadcastAttemptCounter: must(meter.Int64Counter("f3_gpbft_rebroadcast_attempt_counter", metric.WithDescription("Number of rebroadcast attempts"))),
+		errorCounter:              must(meter.Int64Counter("f3_gpbft_error_counter", metric.WithDescription("Number of errors"))),
 	}
 )
-
-func init() {
-	metrics.phaseCounter = must(meter.Int64Counter("f3_gpbft_phase_counter", metric.WithDescription("Number of times phases change")))
-	metrics.roundHistogram = must(meter.Int64Histogram("f3_gpbft_round_histogram",
-		metric.WithDescription("Histogram of rounds per instance"),
-		metric.WithExplicitBucketBoundaries(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 50.0, 100.0, 1000.0),
-	))
-	metrics.broadcastCounter = must(meter.Int64Counter("f3_gpbft_broadcast_counter", metric.WithDescription("Number of broadcasted messages")))
-	metrics.reBroadcastCounter = must(meter.Int64Counter("f3_gpbft_rebroadcast_counter", metric.WithDescription("Number of rebroadcasted messages")))
-	metrics.reBroadcastAttemptCounter = must(meter.Int64Counter("f3_gpbft_rebroadcast_attempt_counter", metric.WithDescription("Number of rebroadcast attempts")))
-	metrics.errorCounter = must(meter.Int64Counter("f3_gpbft_error_counter", metric.WithDescription("Number of errors")))
-}
 
 func metricAttributeFromError(err error) attribute.KeyValue {
 	var v string
