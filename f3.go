ce41e39a563597ef0e816e9df2bbac020725b4b6
Phi <orjan.roren@gmail.com>
2024-08-08 08:25:58 +0200

    chore: Fix typo in `waiting for` log
    chore: Fix typo in `waiting for` log

--- f3.go
+++ f3.go
@@ -223,7 +223,7 @@ func (m *F3) Start(startCtx context.Context) (_err error) {
 				return err
 			} else if delay > 0 {
 				manifestChangeTimer.Reset(delay)
-				log.Infow("waiting for boostrap epoch", "duration", delay.String())
+				log.Infow("waiting for bootstrap epoch", "duration", delay.String())
 			} else {
 				if err := m.reconfigure(m.runningCtx, pendingManifest); err != nil {
 					return fmt.Errorf("failed to reconfigure F3: %w", err)
