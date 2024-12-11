b93a2823caddb4df64d950848cb00c6ea214ca05
Jakub Sztandera <oss@kubuxu.com>
2024-12-11 17:33:42 +0100

    Change ChainMaxLen to 128
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- gpbft/chain.go
+++ gpbft/chain.go
@@ -21,8 +21,7 @@ const (
 	// CidMaxLen specifies the maximum length of a CID.
 	CidMaxLen = 38
 	// ChainMaxLen specifies the maximum length of a chain value.
-	ChainMaxLen = 100
-
+	ChainMaxLen = 128
 	// ChainDefaultLen specifies the default length of chain value.
 	ChainDefaultLen = 100
 	// TipsetKeyMaxLen specifies the maximum length of a tipset. The max size is
