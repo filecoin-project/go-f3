f45804069146fda44c1e6f1717c7e018e3105313
Jakub Sztandera <oss@kubuxu.com>
2025-05-20 17:11:01 +0200

    fix(tests): correct committee lookback index calculation in GetCommittee (#995)
    * fix: Correct committee lookback index calculation in GetCommittee
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * add extra check
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    * add note that certchain is testing utility to avoid further reports
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>
    
    ---------
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- certchain/certchain.go
+++ certchain/certchain.go
@@ -1,3 +1,6 @@
+// certchain package is a testing utility for generating and validating
+// finality certificates.
+// It has no effect on the consensus protocol.
 package certchain
 
 import (
@@ -50,7 +53,10 @@ func (cc *CertChain) GetCommittee(ctx context.Context, instance uint64) (*gpbft.
 	if instance < cc.m.InitialInstance+cc.m.CommitteeLookback {
 		committeeEpoch = cc.m.BootstrapEpoch - cc.m.EC.Finality
 	} else {
-		lookbackIndex := instance - cc.m.CommitteeLookback + 1
+		lookbackIndex := instance - cc.m.CommitteeLookback - cc.m.InitialInstance + 1
+		if lookbackIndex >= uint64(len(cc.certificates)) {
+			return nil, fmt.Errorf("no prior finality certificate to get committee at instance %d", instance)
+		}
 		certAtLookback := cc.certificates[lookbackIndex]
 		committeeEpoch = certAtLookback.ECChain.Head().Epoch
 	}
